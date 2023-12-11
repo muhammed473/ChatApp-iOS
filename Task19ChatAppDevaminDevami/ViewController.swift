dd4b648acb55c42f628c56517b0d26c4a486926f
muhammed dursun <mamiankara10@gmail.com>
2023-12-12 02:20:57 +0300

    12 ARALIK SALI projede yapılan hata SONUCU Proje 8 'inci video olan Google İle Giriş entegrasyonunda kalınmıştır.

---
+++ Task19ChatAppDevaminDevami/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task19ChatAppDevaminDevami
+//
+//  Created by muhammed dursun on 12.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
